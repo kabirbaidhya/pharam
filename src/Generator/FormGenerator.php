1169c6de5bc31a9d639b6a85a18487c9c94e6beb
Saugat Acharya <mesaugat@gmail.com>
2015-07-04 08:40:37 +0545

    Pretty Print HTML

--- src/Generator/FormGenerator.php
+++ src/Generator/FormGenerator.php
@@ -11,14 +11,21 @@ class FormGenerator extends AbstractFormGenerator
     public function generate()
     {
         $html = get_header();
-        $html.="<form>";
+        $html .= "<form>";
         foreach ($this->getElements() as $element) {
             $html .= get_container($element);
         }
-        $html.=get_submit();
-        $html.="</form>";
-        $html.=get_footer();
+        $html .= get_submit();
+        $html .= "</form>";
+        $html .= get_footer();
 
-        return $html;
+        return $this->formatHtml($html);
+    }
+
+    protected function formatHtml($html)
+    {
+        $formatter = $this->getContainer()->make('formatter');
+
+        return $formatter->HTML($html);
     }
 }

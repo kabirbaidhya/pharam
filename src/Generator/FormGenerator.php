e48df158e82903b8b804db32e9a5dcf13d1e0a53
sanjayshiwakoti <sanjayshiwakoti@lftechnology.com>
2015-07-04 07:32:16 +0545

    form tag added.

--- src/Generator/FormGenerator.php
+++ src/Generator/FormGenerator.php
@@ -11,10 +11,12 @@ class FormGenerator extends AbstractFormGenerator
     public function generate()
     {
         $html = get_header();
+        $html.="<form>";
         foreach ($this->getElements() as $element) {
             $html .= get_container($element);
         }
         $html.=get_submit();
+        $html.="</form>";
         $html.=get_footer();
 
         return $html;
